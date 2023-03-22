0c5551117833b65408e9d93d52a0b3213e9ecf54
AidenMiller123 <123018143+AidenMiller123@users.noreply.github.com>
2023-03-22 11:48:25 -0400

    Added slash in types

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,11 +15,26 @@ var displayCard = function (data) {
 	var imgEl = document.createElement("img");
 	imgEl.src = data.sprites.front_default;
 
-	cardEl.style.backgroundColor = '#ADD8E6';
+	var idEl = document.createElement("div");
+	idEl.textContent = "Pokedex # : " + data.id;
 
 	pokeContainer.append(cardEl);
 	cardEl.append(imgEl);
-}
+	cardEl.append(idEl);
+	var typeEl = document.createElement("type");
+		if (data.types.length === 2) {
+			for (var i = 0; i < data.types.length; i++) {
+			typeEl.textContent = data.types[0].type.name + " / " + data.types[1].type.name;
+			}
+		} else {
+			
+			typeEl.textContent = data.types[0].type.name;
+	
+		}
+		cardEl.append(typeEl)
+
+	cardEl.style.backgroundColor = '#ADD8E6';
+	}
 
 function getAPI() {
 	var userInput = textboxEl.value;
@@ -33,6 +48,7 @@ function getAPI() {
 		.then(function (data) {
 
 			displayCard(data);
+			console.log(data)
 		})
 
 		.catch(function (error) {
